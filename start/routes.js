65813fd3401d29dc51d82c89243af62a70ebc13b
Sean Fried <sean.fried@gmail.com>
2020-08-29 00:09:38 -0600

    Fixed issue with back to top button
    Button state not changing when scrolling in a different breakpoint

--- start/routes.js
+++ start/routes.js
@@ -16,5 +16,7 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route');
 
-Route.post('/', 'ContactrequestController.create');
-Route.get('/', 'ContactrequestController.receive');
+// Route.post('/', 'ContactrequestController.create');
+// Route.get('/', 'ContactrequestController.receive');
+
+Route.on('/').render('master');
